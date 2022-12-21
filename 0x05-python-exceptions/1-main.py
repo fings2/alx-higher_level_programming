b626b4589483ab44a582d60a4bbacf86e9924b48
fings2 <aniraphael09@gmail.com>
2022-12-20 17:11:29 -0800

    Task 1

---
+++ 0x05-python-exceptions/1-main.py
@@ -0,0 +1,179 @@
+89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer89
+-89
+School is not an integer
